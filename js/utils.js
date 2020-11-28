16cbcc2194dd46c63993d6dd80445c56dcbc403e
Cyril RICHARD <crichard@cirilgroup.com>
2020-11-28 20:13:14 +0000

    try to add more secure header + no deps with cdn

--- js/utils.js
+++ js/utils.js
@@ -8,7 +8,13 @@ function loadData(callback) {
     filesToLoad.forEach(function(file) {
       var url = ['.', 'data', dir, file + '.html'].join('/')
       var p = new Promise(function(resolve, reject) {
-        fetch(url).then(function(res) {
+        var headers = {}
+        addSecurityHeader(headers);
+        fetch(url, {
+          headers: headers,
+          referrerPolicy: "same-origin",
+          mode: "same-origin"
+        }).then(function(res) {
           res.text().then(function(data) {
             systemData[dir][file] = data;
             resolve();
@@ -24,3 +30,24 @@ function loadData(callback) {
     console.error(err);
   })
 }
+
+function addSecurityHeader(headers) {
+  headers['X-Frame-Options'] = 'DENY';
+  headers['X-XSS-Protection'] = '1; mode=block';
+  headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload';
+  headers['X-Content-Type-Options'] = 'nosniff';
+  headers['Content-Security-Policy'] = "script-src 'self' https://www.google-analytics.com";
+
+}
+//
+// method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//   mode: 'cors', // no-cors, *cors, same-origin
+//   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//   credentials: 'same-origin', // include, *same-origin, omit
+//   headers: {
+//   'Content-Type': 'application/json'
+//   // 'Content-Type': 'application/x-www-form-urlencoded',
+// },
+// redirect: 'follow', // manual, *follow, error
+//   referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//   body: JSON.stringify(data) // body data type must match "Content-Type" header
